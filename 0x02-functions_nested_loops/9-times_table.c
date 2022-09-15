091a52798c61bf88a7c088fd7dd4459c0d4b34a4
JPOkey <chris.ezikanyi@gmail.com>
2022-09-15 03:10:45 -0700

    d

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,35 +1,36 @@
 #include "main.h"
-
 /**
- * timees_table - print times table
+ * times_table -  a function that prints the 9 times table, starting with 0
+ * rone = row, cone = column, d = digits of current result
+ * Return: times table
+ * add extra space past single digit
  */
 void times_table(void)
 {
-	int i, j, result;
+	int rone, cone, d;
 
-	for (i = 0; i <= 9; i++)
+	for (rone = 0; rone <= 9; rone++)
 	{
-		for (j = 0; j <= 9; j++)
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (cone = 1; cone <= 9; cone++)
 		{
-			result = i * j;
-			if [j != 0]
-			if (result > 9)
+			d = (rone * cone);
+			if ((d / 10) > 0)
 			{
-				_putchar(result/10 + '0');
-				_putchar(result%10 + '0');
+			_putchar((d / 10) + '0');
 			}
 			else
 			{
-				_putchar(result + '0');
+			_putchar(' ');
 			}
-			if (j < 9)
+			_putchar((d % 10) + '0');
+			if (cone < 9)
 			{
-				_putchar(',');
-				_putchar(' ');
-				if (result < 10)
-					_putchar(' ');
+			_putchar(',');
+			_putchar(' ');
 			}
-		
 		}
 		_putchar('\n');
 	}
