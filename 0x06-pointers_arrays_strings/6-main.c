bb81db5f754c6ecb29c667e8d8a9a142622ef023
JPOkey <chris.ezikanyi@gmail.com>
2022-09-28 02:02:32 -0700

    d

--- 0x06-pointers_arrays_strings/6-main.c
+++ 0x06-pointers_arrays_strings/6-main.c
@@ -1,39 +1,17 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * cap_string - capitalize all words of a string
- * @str: string
- * Return: `str`
+ * main - check the code
+ *
+ * Return: Always 0.
  */
-
-char *cap_string(char *str)
+int main(void)
 {
-	int i, c;
-	int trigger;
-	char nots[] = ",;.!?(){}\n\t\" ";
-
-	for (i = 0, trigger = 0; str[i] != '\0'; i++)
-	{
-		if (str[0] > 96 && str[0] < 123)
-			trigger = 1;
-		for (c = 0; nots[c] != '\0'; c++)
-		{
-			if (nots[c] == str[i])
-				trigger = 1;
-		}
-
-		if (trigger)
-		{
-			if (str[i] > 96 && str[i] < 123)
-			{
-				str[i] -= 32;
-				trigger = 0;
-			}
-			else if (str[i] > 64 && str[i] < 91)
-				trigger = 0;
-			else if (str[i] > 47 && str[i] < 58)
-				trigger = 0;
-		}
-	}
-	return (str);
+	char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
+        char *ptr;
+	ptr = cap_string(str);
+        printf("%s", ptr);
+	printf("%s", str);
+        return (0);
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -4,5 +4,6 @@
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
+void print_buffer(char *b, int size);
 
 #endif /* MAIN_H */
