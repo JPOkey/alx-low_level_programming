e307363a943a721ca2b391f573fd61a4a9fa7402
JPOkey <chris.ezikanyi@gmail.com>
2022-10-06 04:22:49 -0700

    cap string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,39 +1,39 @@
 #include "main.h"
 
 /**
- * cap_string - capitalize all words of a string
- * @str: string
- * Return: `str`
+ *cap_string - capitalize all words of a string
+ *@c: character 
+ *Return: nothing
  */
 
-char *cap_string(char *str)
+char *cap_string(char *c)
 {
-	int i, c;
+	int i, k;
 	int trigger;
 	char nots[] = ",;.!?(){}\n\t\" ";
 
-	for (i = 0, trigger = 0; str[i] != '\0'; i++)
+	for (i = 0, trigger = 0; c[i] != '\0'; i++)
 	{
-		if (str[0] > 96 && str[0] < 123)
-			trigger = 1;
-		for (c = 0; nots[c] != '\0'; c++)
-		{
-			if (nots[c] == str[i])
-				trigger = 1;
-		}
+	if (c[0] > 96 && c[0] < 123)
+	trigger = 1;
+	for (k = 0; nots[k] != '\0'; k++)
+	{
+	if (nots[k] == c[i])
+	trigger = 1;
+	}
 		
-		if (trigger)
-		{
-			if (str[i] > 96 && str[i] < 123)
-			{
-				str[i] -= 32;
-				trigger = 0;
-			}	
-			else if (str[i] > 64 && str[i] < 91)
-				trigger = 0;
-			else if (str[i] > 47 && str[i] < 58)
-				trigger = 0;
-			}	
-		}
-		return (str);
+	if (trigger)
+	{
+	if (c[i] > 96 && c[i] < 123)
+	{
+	c[i] -= 32;
+	trigger = 0;
+	}	
+	else if (c[i] > 64 && c[i] < 91)
+	trigger = 0;
+	else if (c[i] > 47 && c[i] < 58)
+	trigger = 0;
+	}	
+	}
+	return (str);
 }
