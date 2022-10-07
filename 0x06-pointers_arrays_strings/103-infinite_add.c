bec445ff79c3d55821cae81baf8d667aef50452f
JPOkey <chris.ezikanyi@gmail.com>
2022-10-07 00:49:02 -0700

    infinite add

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -28,7 +28,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		if (j >= 0)
 			n += n2[j] - '0';
 		if (i < 0 && j < 0 && n == 0)
-	{
+		{
 			break;
 		}
 		m = n / 10;
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,5 +7,6 @@ char *leet(char *);
 void print_buffer(char *b, int size);
 void print_number(int n);
 int _putchar(char c);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 #endif /* MAIN_H */
