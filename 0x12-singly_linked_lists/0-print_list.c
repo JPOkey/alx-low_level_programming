5dce9332c6e3c8a18112932b943e8175394f52c4
JPOkey <chris.eziknayi@gmail.com>
2022-10-20 14:09:55 -0700

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,12 +14,11 @@ size_t print_list(const list_t *h)
 	tmp = h;
 	for (i = 0; tmp; i++)
 	{
-		if (tmp->str == NUL)
+		if (tmp->str == NULL)
 			printf("[0] (nil)\n");
 		else
 			printf("[%u] %s\n", tmp->len, tmp->str);
 		tmp = tmp->next;
-		c++;
 	}
 
 	return (i);
