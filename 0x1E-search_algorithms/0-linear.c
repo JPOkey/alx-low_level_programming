c5a1e0bf734389c048f133d6a8ac4caa89b441c9
JPOkey <chris.ezikanyi@gmail.com>
2023-04-12 23:38:54 -0700

    updated

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,31 @@
+#include "search_algos.h"
+
+/**
+ * linear_search: searches for a value in an array of
+ * integers using the Linear search algorithm
+ *
+ * @array: input array
+ * @size: size of the array (No of elements
+ * @value: value to search in
+ * Return: Always EXIT_SUCCESS
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t y;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+
+	for (y = 0; y < size; y++)
+	{
+		printf("Value checked array[%li] = [%i]\n", y, array[y]);
+		if (array[y] == value)
+		{
+			return (y);
+		}
+	}
+	return (-1);
+}
